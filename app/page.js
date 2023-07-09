ff9f01f159e005744035e618e01af5d58634661d
Tuhin1500 <tc79034@gmail.com>
2023-07-09 12:06:41 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Get started g&nbsp;
           <code className={styles.code}>app/page.js</code>
         </p>
         <div>
@@ -17,7 +17,7 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src="/next.svg"
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
@@ -31,7 +31,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="/vercel.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
